cca607bf45c71a76285377bfcf6e3223d6e06bbf
Gor Muradxanyan <gormuradxanyan@MacBook-Pro-Gor.local>
2025-10-21 14:54:38 +0400

    release: v1.0.0 - Stable release with ApiGateway, Docker && Redis

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -4,27 +4,22 @@ using MMLib.SwaggerForOcelot;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// 1. Загружаем конфигурацию Ocelot из файла configuration.json
+// Downloading configuration Ocelot from file configuration.json
 builder.Configuration.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
-
-// 2. Добавляем сервисы Ocelot в DI контейнер
 builder.Services.AddOcelot(builder.Configuration);
 
-// 3. Добавляем поддержку Swagger для Ocelot
-// Эта библиотека соберет Swagger JSON файлы с нижестоящих сервисов
-// и представит их как единый UI.
+// Adding swagger support for for Ocelot
+// This library collects all configs from all services, and swagger UI become shared 
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
-// 4. Добавляем стандартные сервисы Web API
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 
 var app = builder.Build();
 
-// 5. Настраиваем пайплайн для Swagger UI
 app.UseSwagger();
-// Используем UI от SwaggerForOcelot
 app.UseSwaggerForOcelotUI(opt => {
     opt.PathToSwaggerGenerator = "/swagger/docs";
 }).UseOcelot().Wait();
